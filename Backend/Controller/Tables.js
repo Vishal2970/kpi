bb158cdd9c1eb24572b3845b728ebc75cf912d38
Trainings <Trainings@datanova.co.in>
2024-08-29 10:31:31 +0530

    fixed backend for filter

--- Backend/Controller/Tables.js
+++ Backend/Controller/Tables.js
@@ -2,27 +2,136 @@ const { sql } = require("../config/db");
 const getQueryFromXML = require("../XML/parser/XMLTableParser");
 const fs = require("fs");
 
+// const table = async (req, res) => {
+//   try {
+//     const widgetItems = await getQueryFromXML();
+
+//     const responses = await Promise.all(widgetItems.map(async (widgetItem) => {
+//       try {
+//         const queryResponse = await sql.query(widgetItem.query, widgetItem.parameters);
+//         return {
+//           widgetName: widgetItem.widgetName,
+//           parameters: widgetItem.parameters,
+//           response: queryResponse.recordset,
+//         };
+//       } catch (err) {
+//         console.error(err);
+//         return {
+//           widgetName: widgetItem.widgetName,
+//           parameters: widgetItem.parameters,
+//           error: err.message,
+//         };
+//       }
+//     }));
+
+//     res.send(responses);
+//   } catch (err) {
+//     res.status(500).send({
+//       error: err.message,
+//     });
+//   }
+// };
+
+// const table = async (req, res) => {
+//   try {
+//     const widgetItems = await getQueryFromXML();
+//     const widgetName = req.query.widgetName; // assuming the widgetName is passed as a query parameter
+//     const sharedCondition = req.query.sharedCondition; // assuming the sharedCondition is passed as a query parameter
+
+//     const responses = await Promise.all(widgetItems.map(async (widgetItem) => {
+//       try {
+//         let query = widgetItem.query;
+//         let parameters = widgetItem.parameters;
+
+//         if (widgetName) {
+//           // add the WHERE clause to filter by widgetName
+//           query += ` WHERE widgetName = @widgetName`;
+//           parameters.widgetName = widgetName;
+//         }
+
+//         if (sharedCondition) {
+//           // append the shared condition to the query
+//           if (query.includes('WHERE')) {
+//             query += ` AND ${sharedCondition}`;
+//           } else {
+//             query += ` WHERE ${sharedCondition}`;
+//           }
+//         }
+
+//         const queryResponse = await sql.query(query, parameters);
+//         return {
+//           widgetName: widgetItem.widgetName,
+//           parameters: widgetItem.parameters,
+//           response: queryResponse.recordset,
+//         };
+//       } catch (err) {
+//         console.error(err);
+//         return {
+//           widgetName: widgetItem.widgetName,
+//           parameters: widgetItem.parameters,
+//           error: err.message,
+//         };
+//       }
+//     }));
+
+//     res.send(responses);
+//   } catch (err) {
+//     res.status(500).send({
+//       error: err.message,
+//     });
+//   }
+// };
+
 const table = async (req, res) => {
   try {
     const widgetItems = await getQueryFromXML();
+    const widgetName = req.query.widgetName; // assuming the widgetName is passed as a query parameter
+    const sharedCondition = req.query.sharedCondition; // assuming the sharedCondition is passed as a query parameter
+    console.log(sharedCondition);
 
-    const responses = await Promise.all(widgetItems.map(async (widgetItem) => {
-      try {
-        const queryResponse = await sql.query(widgetItem.query, widgetItem.parameters);
-        return {
-          widgetName: widgetItem.widgetName,
-          parameters: widgetItem.parameters,
-          response: queryResponse.recordset,
-        };
-      } catch (err) {
-        console.error(err);
-        return {
-          widgetName: widgetItem.widgetName,
-          parameters: widgetItem.parameters,
-          error: err.message,
-        };
+    console.log(widgetName);
+
+    // Filter the widgetItems array based on the widgetName parameter
+    const filteredWidgetItems = widgetItems.filter((widgetItem) => {
+      if (widgetName) {
+        return widgetItem.widgetName === widgetName;
       }
-    }));
+      return true; // if no widgetName is provided, return all widgetItems
+    });
+
+    const responses = await Promise.all(
+      filteredWidgetItems.map(async (widgetItem) => {
+        try {
+          let query = widgetItem.query;
+          let parameters = widgetItem.parameters;
+          console.log(query);
+
+          if (sharedCondition) {
+            // append the shared condition to the query
+            if (query.includes("WHERE")||query.includes("Where")||query.includes("where")) {
+              query += ` AND ${sharedCondition}`;
+            } else {
+              query += ` WHERE ${sharedCondition}`;
+            }
+          }
+          console.log("final query " + query);
+
+          const queryResponse = await sql.query(query, parameters);
+          return {
+            widgetName: widgetItem.widgetName,
+            parameters: widgetItem.parameters,
+            response: queryResponse.recordset,
+          };
+        } catch (err) {
+          console.error(err);
+          return {
+            widgetName: widgetItem.widgetName,
+            parameters: widgetItem.parameters,
+            error: err.message,
+          };
+        }
+      })
+    );
 
     res.send(responses);
   } catch (err) {
@@ -31,4 +140,5 @@ const table = async (req, res) => {
     });
   }
 };
-module.exports = {table};
+
+module.exports = { table };
