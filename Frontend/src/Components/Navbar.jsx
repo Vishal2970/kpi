851aa4d516142ead23219da3a65424fdcd8c7425
Trainings <Trainings@datanova.co.in>
2024-08-30 18:29:54 +0530

    Working on filter button

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Box from "@mui/material/Box";
-import logo from "../Images/dataNova.png"; // Ensure the path is correct
+import logo from "../Images/dataNova.png"; 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const NavBar = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    console.log(selectedDate);
+  };
+
   return (
     <AppBar position="static" color="transparent" elevation={0}>
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-label="logo">
-          <Box component="img" sx={{ blockSize: 40 }} alt="Logo"  srcSet={logo} />
+          <Box
+            component="img"
+            sx={{ blockSize: 40 }}
+            alt="Logo"
+            src={logo}
+          />
         </IconButton>
         <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ blockSize: 20 }}>
+          <DatePicker selected={selectedDate} onChange={handleDateChange} dateFormat="dd/MM/yyyy"/>
+        </Box>
       </Toolbar>
     </AppBar>
   );
 };
 
-export default NavBar;//src={logo}
+export default NavBar;
