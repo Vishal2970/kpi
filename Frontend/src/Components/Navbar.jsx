45c6bdbdf89cbc4ca24001fa1289a25035934537
Trainings <Trainings@datanova.co.in>
2024-09-02 16:10:20 +0530

    Added filter button working on it

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -4,16 +4,50 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Box from "@mui/material/Box";
 import logo from "../Images/dataNova.png";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
 
 const NavBar = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const [openModal, setOpenModal] = useState(false);
+  const [openCalendar, setOpenCalendar] = useState(false);
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
-    console.log(selectedDate);
+    setOpenCalendar(false);
   };
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleModalOpen = () => {
+    setOpenModal(true);
+  };
+
+  const handleModalClose = () => {
+    setOpenModal(false);
+  };
+
+  const handleDateClick = () => {
+    setOpenCalendar(true);
+  };
+
   return (
     <AppBar position="static" color="transparent" elevation={0}>
       <Toolbar>
@@ -22,15 +56,75 @@ const NavBar = () => {
         </IconButton>
         <Box sx={{ flexGrow: 1 }} />
         <Box sx={{ blockSize: 20 }}>
-          <DatePicker
-            selected={selectedDate}
-            onChange={handleDateChange}
-            dateFormat="dd/MM/yyyy"
-          />
+          <Button
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            User
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={handleModalOpen}>Filter</MenuItem>
+            <MenuItem>LogOut</MenuItem>
+          </Menu>
+          <Dialog
+            open={openModal}
+            onClose={handleModalClose}
+            sx={{
+              "& .MuiDialogContent-root": {
+                blockSize: "400px", // Adjusted filter model size
+                inlineSize: "500px", // Adjusted filter model size
+              },
+            }}
+          >
+            <DialogTitle>Filter</DialogTitle>
+            <DialogContent>
+              <Grid container spacing={-10}>
+                <Grid item xs={4}>
+                  <Button variant="outlined" fullWidth>
+                    Button 1
+                  </Button>
+                </Grid>
+                <Grid item xs={4}>
+                  <Divider orientation="vertical" />
+                </Grid>
+                <Grid item xs={4}>
+                  <Button
+                    variant="outlined"
+                    fullWidth
+                    onClick={handleDateClick}
+                  >
+                    {selectedDate.toLocaleDateString()}
+                  </Button>
+                  {openCalendar && (
+                    <DatePicker
+                      selected={selectedDate}
+                      onChange={handleDateChange}
+                      dateFormat="dd/MM/yyyy"
+                      inline
+                    />
+                  )}
+                </Grid>
+              </Grid>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleModalClose}>Cancel</Button>
+              <Button onClick={handleModalClose}>OK</Button>
+            </DialogActions>
+          </Dialog>
         </Box>
       </Toolbar>
     </AppBar>
   );
 };
-
 export default NavBar;
